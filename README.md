98c390d7e01665a115be0d379bedeefba966cdc4
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-06-02 09:29:31 -0700

    docs

---
+++ README.md
@@ -0,0 +1,9 @@
+Require path aliasing.
+
+Put `require-paths.json` in your root. And then use
+
+```
+require('ral')('key');
+```
+
+More docs coming soon.
--- ral.js
+++ ral.js
@@ -1,18 +1,13 @@
 'use strict';
 
 var findup  = require('findup-sync'),
-    rootPath = findup('*/require-paths.json'),
-    paths   = require(rootPath);
+    path = require('path'),
+    filePath = findup('*/require-paths.json'),
+    rootPath = path.dirname(filePath),
+    paths   = require(filePath);
 
 module.exports = alias;
 
 function alias (path) {
-    path = 'constants';
-    console.log('--- they are:');
-    console.log(paths);
-    console.log('path to get:')
-    console.log(rootPath + '/' + paths[path]);
-    console.log('--- it is:');
-    console.log(require(rootPath + '/' + paths[path]));
-    console.log('---------------');
+    return require(rootPath + '/' + paths[path]);
 }
